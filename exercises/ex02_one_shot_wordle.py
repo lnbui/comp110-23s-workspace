799e9a2cf5372ada60a08d2a624f82956a08d5dd
Le Bui <lnbui2002@gmail.com>
2023-02-15 21:35:02 -0500

    editing for linting

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,10 +3,10 @@
 __author__ = "730398384"
 
 secret: str = "python"
-guess: str = input(f"What is your {len(secret)}-letter guess? ")  #getting the word input and ensuring the length of the word is correct
-normalized_guess: str = guess.lower()  #ensuring that capitalization doesn't matter
+guess: str = input(f"What is your {len(secret)}-letter guess? ")  # getting the word input and ensuring the length of the word is correct
+normalized_guess: str = guess.lower()  # ensuring that capitalization doesn't matter
 
-WHITE_BOX: str = "\U00002B1C"  #declaring box colors for correct or incorrect boxes
+WHITE_BOX: str = "\U00002B1C"  # declaring box colors for correct or incorrect boxes
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 letter_box = ""
@@ -15,13 +15,13 @@ i: int = 0
 
 while i < len(secret):
     if len(secret) != len(normalized_guess):
-        guess: str = input(f"That was not {len(secret)} letters! Try again! ") #ensuring guess is as long as secret
-        normalized_guess: str = guess.lower()
+        guess = input(f"That was not {len(secret)} letters! Try again! ") # ensuring guess is as long as secret
+        normalized_guess = guess.lower()
     elif secret[i] == normalized_guess[i]:
         letter_box = letter_box + GREEN_BOX
     elif secret[i] != normalized_guess[i]:
         j = 0
-        letter_match = "False" #if index of guess doesn't match up to secret then it goes into the loop
+        letter_match = "False" # if index of guess doesn't match up to secret then it goes into the loop
         while j < len(secret):
             if secret[j] == normalized_guess[i]:
                 letter_match = "True"
@@ -32,7 +32,7 @@ while i < len(secret):
             letter_box = letter_box + WHITE_BOX
     i += 1
 
-if len(secret) == len(normalized_guess):  #if guess is the secret than "Woo! you got it! appears, and if not, then "Not quite. Play again soon!" does
+if len(secret) == len(normalized_guess):  # if guess is the secret than "Woo! you got it! appears, and if not, then "Not quite. Play again soon!" does
     if secret == normalized_guess:
         print(letter_box)
         print("Woo! You got it!")
